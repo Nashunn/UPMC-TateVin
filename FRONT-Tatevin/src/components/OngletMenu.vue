2dc4b870a72b168ba8ecb07e1af1840e2ac6cddf
LauraPerinet <lpmarquet@gmail.com>
2018-06-19 16:36:22 +0200

    Des carrés git add FRONT-Tatevin/src/components/OngletMenu.vue !

---
+++ FRONT-Tatevin/src/components/OngletMenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <li :class="className" v-bind:style="getSize">
+        <a :href="link">{{ textOnglet }}</a>
+    </li>
+</template>
+<script>
+export default {
+  name: 'ongletMenu',
+  props:{
+      className:{type:String},
+      link:{type:String},
+      textOnglet:{type:String},
+  },
+  computed:{
+      getSize:function(){
+          var size=screen.height/10;
+          return {"height":size+"px", "width":size+"px", "margin":size*0.1}
+      }
+  }
+}
+
+</script>
