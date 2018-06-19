6105cee2eaf73b0f06720368241df4da7a212477
LauraPerinet <lpmarquet@gmail.com>
2018-06-19 14:18:41 +0200

    menu header en cours

--- FRONT-Tatevin/src/main.js
+++ FRONT-Tatevin/src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
-
+import './styles/style.css';
+import './styles/fonts.css';
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
