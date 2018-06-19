ac2b9dd6b6c3248f9bf3ee6ea72e1a55a93ec196
Nashunn <boullet.nicolas@gmail.com>
2018-06-19 17:35:35 +0200

    auto

---
+++ FRONT-Tatevin/src/auth/index.js
@@ -0,0 +1,122 @@
+import { HTTP } from "./../http/http";
+import auth from "./../auth/index";
+//import {EventBusModal} from "../events/event-modals";
+
+export default {
+    user: {},
+
+    login(context, creds, redirect) {
+        var that = this;
+
+        HTTP.post(
+            "/login",
+            {
+                email: creds.username,
+                password: creds.password
+            }
+        )
+            .then( async response => {
+                console.log(response.data);
+                await localStorage.setItem("id_token", response.data.token);
+                that.getUserData(context)
+                //EventBusModal.$emit("change-state-login", false)
+                if (redirect) {
+                    //context.$router.replace("/dashboard"); //modal = false
+                    //context.$showModal = false;
+                    // should be : context.$router.replace("/");
+                }
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    },
+
+    signup(context, creds, redirect) {
+        HTTP.post(
+            "/register",
+            {
+                email: creds.email,
+                username: creds.username,
+                firstName: creds.firstName,
+                lastName: creds.lastName,
+                password: creds.password,
+                passwordConf: creds.passwordConf,
+                avatar: creds.avatar
+            },
+            {
+                //headers: auth.getAuthHeader()
+            }
+        )
+            .then( async response => {
+                console.log(response.data);
+                await localStorage.setItem("id_token", response.data.token);
+                that.getUserData(context)
+                //EventBusModal.$emit("change-state-login", false)
+                if (redirect) {
+
+                    //context.$router.replace("/");
+                    // should be : context.$router.replace("/");
+                }
+            })
+            .catch(function(error) {
+                return error;
+                console.log(error);
+            });
+    },
+
+    getUserData(context){
+        HTTP.get('account', { headers: {
+                Authorization: "Bearer " + localStorage.getItem('id_token'),
+                authorization: localStorage.getItem('id_token'),
+                'x-access-token': localStorage.getItem('id_token'),
+                Accept: "application/json"
+            }}).then( async response => {
+            console.log(response.data)
+            await context.$store.commit("instanceUser", response.data);
+        });
+    },
+
+    signupErasable(context, creds, redirect) {
+        context.$http
+            .post(SIGNUP_URL, creds, data => {
+                localStorage.setItem("id_token", data.id_token);
+
+                this.user.authenticated = true;
+
+                if (redirect) {
+                }
+            })
+            .error(err => {
+                context.error = err;
+            });
+    },
+
+    logout(context) {
+        localStorage.removeItem("id_token");
+        context.$store.commit("destroyUser");
+
+        this.user.authenticated = false;
+    },
+
+    onChange() {},
+
+    checkAuth() {
+        var jwt = localStorage.getItem("id_token");
+        if (jwt) {
+            this.user.authenticated = true;
+        } else {
+            this.user.authenticated = false;
+        }
+    },
+
+    loggedIn() {
+        console.log(localStorage.getItem("id_token"));
+        return localStorage.getItem("id_token");
+    },
+
+    getAuthHeader() {
+        return {
+            Authorization: "Bearer " + localStorage.getItem("id_token")
+        };
+    }
+};
--- FRONT-Tatevin/src/components/Signup.vue
+++ FRONT-Tatevin/src/components/Signup.vue
@@ -3,57 +3,76 @@
         <h2>Signup</h2>
 
         <form @submit.prevent="validateBeforeSubmit()" class="content row col s10 offset-s1 m9 offset-m1">
-            <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"
-                          @vdropzone-complete="afterComplete" />
-            <p class="row">
-                <label for="pseudo">Pseudo : </label>
-                <input v-model="credentials.username" type="text" id="pseudo" class="tb-input" required/>
+            <p class="">
+                <label for="username">Username : </label>
+                <input v-model="credentials.username" type="text" id="username" required/>
             </p>
-            <p class="row">
-                <label for="pseudo">Nom : </label>
-                <input v-model="credentials.lastName" type="text" id="lastname" class="tb-input" required/>
+            <p class="">
+                <label for="username">Email : </label>
+                <input v-model="credentials.email" type="email" id="email" required/>
             </p>
-            <p class="row">
-                <label for="pseudo">Prénom : </label>
-                <input v-model="credentials.firstName" type="text" id="firstname" class="tb-input" required/>
+            <p class="">
+                <label for="password">Password : </label>
+                <input v-model="credentials.password" type="text" id="password" required/>
             </p>
-            <p class="row">
-                <label for="mail">Email : </label>
-                <input v-model="credentials.email" type="email" id="mail" class="tb-input" required/>
+            <p class="">
+                <label for="passwordconf">Password Confirmation : </label>
+                <input v-model="credentials.username" type="text" id="passwordconf" required/>
             </p>
-            <p class="row">
-                <label for="pwd">Mot de passe : </label>
-                <input v-model="credentials.password" type="password" id="pwd" class="tb-input" required/>
+            <p class="">
+                <label for="birthdate">Birth date : </label>
+                <input v-model="credentials.birthdate" type="date" id="birthdate" required/>
             </p>
-            <p class="row">
-                <label for="pwdConfirm">Confirmation mot de passe : </label>
-                <input v-model="credentials.passwordConf" @change="chkPass" type="password" id="pwdConfirm" class="tb-input" required/>
+            <p>
+                <vue-dropzone
+                    ref="userAvatar"
+                    id="dropzone"
+                    :options="dropzoneOptions"
+                    @vdropzone-complete="afterComplete"
+                />
             </p>
-            <p class="row">
-                <label for="city">Ville : </label>
-                <input v-model="credentials.city" type="text" id="city" class="tb-input"/>
-            </p>
-            <p class="row">
-                <label for="country">Pays : </label>
-                <input v-model="credentials.country" type="text" id="country" class="tb-input"/>
-            </p>
-            <div class="col s12 m12">
-                <button type="submit" ref="btnSubmit" class="btn-next tb-btn btn-red right">S'inscrire</button>
-                <button @click="close()" class="btn-close tb-btn btn-red right">Fermer</button>
+            <div class="btn-wrapper">
+                <button type="submit" ref="btnSubmit">S'inscrire</button>
             </div>
-            <p>{{passInfo}}</p>
-
-            <p></p>
+            <p>{{passMsg}}</p>
         </form>
     </section>
 </template>
 
 <script>
+    import {HTTP} from "../HTTP/http";
+    import auth from "../auth/index"
+    import vue2Dropzone from "vue2-dropzone";
+    import "vue2-dropzone/dist/vue2Dropzone.css";
+
     export default {
         name: 'Signup',
+        components: {
+            vueDropzone: vue2Dropzone
+        },
         data () {
             return {
-
+                dropzoneOptions: {
+                    url: "https://httpbin.org/post",
+                    thumbnailWidth: 150,
+                    maxFiles: 1,
+                    maxFilesize: 0.5,
+                    addRemoveLinks: true,
+                    headers: { "My-Awesome-Header": "header value" },
+                    accept: function accept(file, done) {
+                        done();
+                    }
+                },
+                passMsg:'',
+                credentials: {
+                    avatar: "",
+                    username: "",
+                    pseudo: "",
+                    email: "",
+                    birthdate: "",
+                    password: "",
+                    passwordConf: ""
+                },
             }
         },
         methods: {
@@ -61,6 +80,44 @@
                 //add more security
                 this.submit();
             },
+            afterComplete(file) {
+                console.log(file);
+                this.credentials.avatar = file.dataURL;
+            },
+            chkPass() {
+                if (this.credentials.password !== this.credentials.passwordConf) {
+                    this.passMsg = "Passwords don't match !";
+                    this.$refs.btnSubmit.disabled = true;
+                } else {
+                    this.passMsg = "Passwords matches !";
+                    this.$refs.btnSubmit.disabled = false;
+                }
+            },
+            submit() {
+                var that = this;
+                console.log(this.credentials);
+                var p1 = new Promise(function(resolve, reject) {
+                    resolve(auth.signup(this, that.credentials, "secretquote"));
+                });
+
+                p1
+                .then(function(value) {
+                    console.log(value); // "Succès!"
+                    HTTP.get("/account").then(response => {
+                        let user = response.data;
+                        that.$store.commit("instanceUser",user);
+                        console.log(user)
+                        EventBusModal.$emit('usr-loaded', user);
+                    });
+                    throw "Inscription impossible";
+                })
+                .catch(function(e) {
+                    console.error(e);
+                })
+                .then(function(e) {
+                    that.error = e;
+                });
+            }
         }
     }
 </script>
