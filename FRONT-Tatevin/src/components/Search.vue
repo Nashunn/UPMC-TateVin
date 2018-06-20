807f84df9463444a8fe6eef68d5cfffc08a0b8b4
Nashunn <boullet.nicolas@gmail.com>
2018-06-20 17:29:56 +0200

    add btn to create wine in research component

--- FRONT-Tatevin/src/components/Search.vue
+++ FRONT-Tatevin/src/components/Search.vue
@@ -1,26 +1,45 @@
 <template>
-  <div class="hello">
-  </div>
+    <section class="hello">
+        <h2>Recherche</h2>
+        <div class="search-wrapper">
+            <input v-model="searchValue" @keyup.enter="doSearch()" type="text"/>
+            <div class="btn-wrapper">
+                <button @click="doSearch()" class="btn-next tb-btn btn-red right">Rechercher</button>
+            </div>
+        </div>
+
+        <div class="">
+            <button @click="displayCreateWine()" class="btn-next tb-btn btn-red right">Nouveau vin</button>
+        </div>
+
+    </section>
 </template>
 
 <script>
-  import { EventBusModal } from "./../events/";
+    import {EventBusModal} from "./../events/";
 
-  export default {
-    name: 'hello',
-    data () {
-      return {
-      }
-    },
-    mounted(){
-      EventBusModal.$on('loading', loading => {
-        alert("coucou")
-      })
-    },
-    methods:{
-      test() {
-        EventBusModal.$emit("loading", true);
-      }
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                searchValue: "",
+            }
+        },
+        created() {
+            EventBusModal.$on('loading', loading => {
+                alert("coucou")
+            })
+        },
+        methods: {
+            test() {
+                EventBusModal.$emit("loading", true);
+            },
+            doSearch() {
+                alert("Todo");
+            },
+            displayCreateWine() {
+                alert("Todo Nico");
+            }
+        }
     }
-  }
 </script>
