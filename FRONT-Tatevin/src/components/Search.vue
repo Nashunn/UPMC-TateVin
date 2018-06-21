d228ae1cf07a2bafe22e638346dfddf590ef4272
Nashunn <boullet.nicolas@gmail.com>
2018-06-21 12:31:39 +0200

    pushing for tim

--- FRONT-Tatevin/src/components/Search.vue
+++ FRONT-Tatevin/src/components/Search.vue
@@ -3,14 +3,35 @@
         <h2>Recherche</h2>
         <p>Affiner votre recherche</p>
 
-        <div class="search-wrapper">
+        <b-card class="search-wrapper">
+            <b-form-group label="Catégories : ">
+                <b-form-checkbox-group v-model="search.categories" name="category" :options="optionsCat">
+                </b-form-checkbox-group>
+            </b-form-group>
 
+    
+            <p class="m4">
+                <label for="tags">Mots clés : </label>
+                <input v-model="search.tags" id="tags" type="text"/>
+            </p>
 
-            <input v-model="searchValue" @keyup.enter="doSearch()" type="text"/>
+            <p class="m4">
+                <label for="domaine">Domaine : </label>
+                <input v-model="search.domaine" id="domaine" type="text"/>
+            </p>
+            <p class="m4">
+                <label for="terroir">Terroir : </label>
+                <input v-model="search.terroir" id="terroir" type="text"/>
+            </p>
+            <p class="m4">
+                <label for="millesime">Millésime : </label>
+                <input v-model="search.millesime" id="millesime" type="number"/>
+            </p>
+
             <div class="btn-wrapper">
                 <button @click="doSearch()" class="wine-btn btn-purple">Rechercher</button>
             </div>
-        </div>
+        </b-card>
 
         <div class="">
             <button @click="displayCreateWine()" class="wine-btn btn-purple">Nouveau vin</button>
@@ -26,7 +47,20 @@
         name: 'hello',
         data() {
             return {
-                searchValue: "",
+                search: {
+                    tags: [],
+                    categories: [],
+                    domaine: "",
+                    terroir: "",
+                    millesime: "",
+                },
+                optionsCat: [
+                    {text: 'Vin', value: 'vin'},
+                    {text: 'Histoire de vin', value: 'hdv'},
+                    {text: 'Apprentissage', value: 'apprentissage'},
+                    {text: 'Utilisateur', value: 'utilisateur'},
+                    {text: 'Producteur', value: 'producteur'},
+                ]
             }
         },
         created() {
